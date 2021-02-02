1afa60058410da78d210a7caad387378b4d6bf52
Nour <nour.eldin.shobier@gmail.com>
2021-02-02 14:37:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dartplus
+ 
---
+++ lib/dartplus.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dartplus;
+
+export 'src/dartplus_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
